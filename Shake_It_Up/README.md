8ea6e7c9c82da80de4bbc230a528b6ce205b506c
miaapanda <balla.anamihaela01@gmail.com>
2023-11-06 20:55:44 +0200

    Project Update

---
+++ Shake_It_Up/README.md
@@ -0,0 +1,32 @@
+https://github.com/MiaaPanda/pythonProject3/blob/master/Shake_It_Up/Arhitecture%20Diagram%20.jpg
+
+Flow Diagram
+1. Start
+2. Setup Tkinter GUI: Create window, frames, labels, buttons and entry fields.
+3. Input Ingredients: User inputs ingredients into the entry fields.
+4. Search Cocktails: On clicking the "Search Cocktails" button, the search_cocktail function is called.
+        - print "Searching cocktails"
+        - Get the ingredient from the entry field
+        - Call search_cocktails_by_ingredient function for each ingredient:
+            * Print "Searching Cocktails with ingredient: {ingredient}"
+            * Construct the API URL with the ingredient
+            * Send API request to get cocktails with the given ingredient.
+            * Receive API response and decode the JSON data.
+            * If "drinks" are present in the data, return the list of drinks.
+            * If an error occurs during JSON decoding, print an error message and return an empty list.
+        - Display the list of cocktails in the result text box or display "No cocktails found" if the list ist empty.
+5. Select Cocktail: User selects a cocktail from the list.
+6. Display Cocktail Details: On clicking the "Display Details" button, the display_cocktail_details function is called:
+         - Print "Displaying cocktail details".
+         - Get the selected cocktail name from the list box.
+         - Find the ID of the selected cocktail.
+         - Call search_cocktail_details function with the ID of the selected cocktail:
+            * Print "Fetching details for cocktail ID: {cocktail_id}
+            * Construct the API URL with the cocktail ID
+            * Send API request to get details of the selected cocktail
+            * Receive API responde and decode JSON data.
+            * Return the details of the cocktail.
+         - Display the cocktail details (ingredients, instructions, image) in the respective fields.
+7. End 
+
+https://github.com/MiaaPanda/pythonProject3/blob/master/Shake_It_Up/FlowDiagram.JPG
--- main.py
+++ main.py
@@ -5,9 +5,10 @@ import requests
 from io import BytesIO
 
 BASE_API_URL = "https://www.thecocktaildb.com/api/json/v1/1/"
+all_cocktails = []
 
-all_cocktails = []  # Define all_cocktails as a global list to store cocktails
 def search_cocktails_by_ingredient(ingredient):
+    print(f"Searching cocktails with ingredient: {ingredient}")
     search_url = BASE_API_URL + "filter.php?i=" + ingredient
     response = requests.get(search_url)
     try:
@@ -21,24 +22,23 @@ def search_cocktails_by_ingredient(ingredient):
         return []
 
 def search_cocktail_details(cocktail_id):
-    # Construct the URL to retrieve details of a specific cocktail by its ID
+    print(f"Fetching details for cocktail ID: {cocktail_id}")
     cocktail_url = BASE_API_URL + "lookup.php?i=" + cocktail_id
     response = requests.get(cocktail_url)
     data = response.json()
     return data["drinks"][0]
 
 def display_cocktail_details():
+    print("Displaying cocktail details  ")
     selected_index = result_text.curselection()
     if selected_index:
         selected_cocktail_name = result_text.get(selected_index)
-        # Find the selected cocktail ID based on its name
         for cocktail in all_cocktails:
             if cocktail["strDrink"] == selected_cocktail_name:
                 selected_cocktail_id = cocktail["idDrink"]
                 break
         cocktail_details = search_cocktail_details(selected_cocktail_id)
         if cocktail_details:
-            # Display ingredients, instructions, and image
             ingredients_text.delete(1.0, tk.END)
             ingredients_text.insert(tk.END, "Ingredients:\n")
             for i in range(1, 16):
@@ -46,22 +46,18 @@ def display_cocktail_details():
                 measure = cocktail_details[f"strMeasure{i}"]
                 if ingredient:
                     ingredients_text.insert(tk.END, f"{measure} {ingredient}\n")
-
             instructions_text.delete(1.0, tk.END)
             instructions_text.insert(tk.END, f"Instructions:\n{cocktail_details['strInstructions']}\n")
-
             image_url = cocktail_details['strDrinkThumb']
             image_label.config(text="Image:")
-
             try:
                 image_data = requests.get(image_url).content
                 image = Image.open(BytesIO(image_data))
                 photo = ImageTk.PhotoImage(image)
                 image_label.config(image=photo)
-                image_label.image = photo  # Keep a reference to the image to avoid garbage collection
+                image_label.image = photo
             except Exception as e:
                 print(f"Failed to load the image: {e}")
-
         else:
             ingredients_text.delete(1.0, tk.END)
             instructions_text.delete(1.0, tk.END)
@@ -72,21 +68,16 @@ def display_cocktail_details():
         image_label.config(text="Image:")
 
 def search_cocktails():
-    global all_cocktails  # Declare all_cocktails as a global variable
+    print("Searching cocktails")
+    global all_cocktails
     ingredient1 = ingredient1_entry.get()
-    ingredient2 = ingredient2_entry.get()
     all_cocktails.clear()
     cocktails1 = search_cocktails_by_ingredient(ingredient1)
-    cocktails2 = search_cocktails_by_ingredient(ingredient2)
-    if not cocktails1 and not cocktails2:
+    if not cocktails1:
         result_text.delete(0, tk.END)
-        result_text.insert(tk.END, "No cocktails found for the given ingredients.")
+        result_text.insert(tk.END, "No cocktails found")
         return
-    # Intersect the results to get cocktails that contain both ingredients
-    cocktails1_ids = set(cocktail["idDrink"] for cocktail in cocktails1)
-    cocktails2_ids = set(cocktail["idDrink"] for cocktail in cocktails2)
-    common_ids = cocktails1_ids.intersection(cocktails2_ids)
-    all_cocktails.extend(cocktail for cocktail in cocktails1 if cocktail["idDrink"] in common_ids)
+    all_cocktails.extend(cocktail for cocktail in cocktails1)
     if all_cocktails:
         result_text.delete(0, tk.END)
         for cocktail in all_cocktails:
@@ -95,17 +86,32 @@ def search_cocktails():
         result_text.delete(0, tk.END)
         result_text.insert(tk.END, "No cocktails found")
 
+    ingredient2 = ingredient2_entry.get()
+    if ingredient2:
+        cocktails2 = search_cocktails_by_ingredient(ingredient2)
+        if not cocktails2:
+            return
+        cocktails1_ids = set(cocktail["idDrink"] for cocktail in cocktails1)
+        cocktails2_ids = set(cocktail["idDrink"] for cocktail in cocktails2)
+        common_ids = cocktails1_ids.intersection(cocktails2_ids)
+        all_cocktails.clear()
+        all_cocktails.extend(cocktail for cocktail in cocktails1 if cocktail["idDrink"] in common_ids)
+        if all_cocktails:
+            result_text.delete(0, tk.END)
+            for cocktail in all_cocktails:
+               result_text.insert(tk.END, cocktail["strDrink"])
+        else:
+            result_text.delete(0, tk.END)
+            result_text.insert(tk.END, "No common cocktails found")
 
 window = tk.Tk()
 window.title("Cocktail Search")
 window.iconbitmap('cocktail.ico')
 window.configure(bg='light blue')
 
-# Create a frame for the search section
 search_frame = tk.Frame(window)
 search_frame.grid(row=0, column=0, columnspan=2)
 
-# Create labels and entry widgets for entering ingredients
 ingredient1_label = tk.Label(search_frame, text="Ingredient 1:")
 ingredient1_label.pack(side=tk.LEFT)
 ingredient1_entry = tk.Entry(search_frame)
@@ -116,35 +122,27 @@ ingredient2_label.pack(side=tk.LEFT)
 ingredient2_entry = tk.Entry(search_frame)
 ingredient2_entry.pack(side=tk.LEFT)
 
-# Create a search button
 search_button = tk.Button(search_frame, text="Search Cocktails", command=search_cocktails)
 search_button.pack(side=tk.LEFT)
 
-# Create a listbox to display the results
 result_text = tk.Listbox(window, width=40, height=10)
 result_text.grid(row=1, column=0, padx=10, pady=10)
 
-# Open the image file
 img = Image.open('button.ico')
-# Resize the image if necessary
 img = img.resize((50, 50), Image.LANCZOS)
-# Convert the image to a PhotoImage
+
 photo = ImageTk.PhotoImage(img)
 
-# Now you can use 'photo' to set the image of the button
 display_button = tk.Button(window, text="Display Details", command=display_cocktail_details, image=photo, compound=tk.LEFT)
-display_button.grid(row=2, column=0, padx=10, pady=10)  # Change column to 0
+display_button.grid(row=2, column=0, padx=10, pady=10)
 
-# Create labels for displaying cocktail details
 ingredients_text = tk.Text(window, width=40, height=10)
 ingredients_text.grid(row=1, column=1, padx=10, pady=10)
 
 instructions_text = tk.Text(window, width=40, height=10)
 instructions_text.grid(row=2, column=1, padx=10, pady=10)
 
-# Create an image label
 image_label = tk.Label(window, text="Image:")
-image_label.grid(row=1, column=2, rowspan=2)  # Change column to 2
+image_label.grid(row=1, column=2, rowspan=2)
 
 window.mainloop()
-
